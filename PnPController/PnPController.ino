acf85b5eae44c7e889522ff674b6c95f397a2fa0
Sanskar Biswal <sb102@ikoop.privat>
2024-04-29 17:21:37 +0200

    - added function to control valve - added function to control EN - MOTOR 2 and 3 definitions - Setup Alternate Serial Pins

--- PnPController/PnPController.ino
+++ PnPController/PnPController.ino
@@ -11,7 +11,8 @@
 #define MOTOR_MICROSTEP         16
 #define MOTOR_CURRENT           300
 
-#define MOTOR_1
+#define MOTOR_1                 // Z-Alignment Stepper Motor
+#define MOTOR_2                 // Hollow-Shaft Stepper Motor
 
 #ifdef MOTOR_1
 SoftwareSerial m1_uart(M1_UART_RX, M1_UART_TX); // RX, TX
@@ -35,7 +36,10 @@ SpeedyStepper stepper3;
 void init_stepper_motors(void);
 void enable_motor(int);
 void disable_motor(int);
-void rotate_motor_by_anagle(SpeedyStepper, float);
+void valve_ON(void);
+void valve_OFF(void);
+void rotate_motor1_by_angle(float);
+void rotate_motor2_by_angle(float);
 
 
 /**
@@ -47,7 +51,7 @@ void init_stepper_motors(){
   pinMode(M1_DIR, OUTPUT);
   pinMode(M1_EN, OUTPUT);
 
-  pinMode(M1_EN, HIGH);
+  pinMode(M1_EN, HIGH); // Hardware Enable 
   stepper1.connectToPins(M1_STEP, M1_DIR);
 
   driver1.begin();
@@ -55,22 +59,81 @@ void init_stepper_motors(){
   driver1.rms_current(MOTOR_CURRENT);
   driver1.microsteps(MOTOR_MICROSTEP);
 
-  // driver.pwm_autoscale(true);    // Needed for stealthChop
-  // driver.en_spreadCycle(true);   // false = StealthChop / true = SpreadCycle
+  // driver1.pwm_autoscale(true);    // Needed for stealthChop
+  // driver1.en_spreadCycle(true);   // false = StealthChop / true = SpreadCycle
   driver1.en_spreadCycle(false);
 
   stepper1.setCurrentPositionInSteps(0);
   stepper1.setSpeedInStepsPerSecond(400);
   stepper1.setAccelerationInStepsPerSecondPerSecond(400);
   #endif
+  
+  #ifdef MOTOR_2
+  pinMode(M2_STEP, OUTPUT);
+  pinMode(M2_DIR, OUTPUT);
+  pinMode(M2_EN, OUTPUT);
+
+  pinMode(M2_EN, HIGH); // Hardware Enable 
+  stepper2.connectToPins(M2_STEP, M2_DIR);
+
+  driver2.begin();
+  driver2.toff(5);          // Enables Driver in Software
+  driver2.rms_current(MOTOR_CURRENT);
+  driver2.microsteps(MOTOR_MICROSTEP);
+
+  // driver2.pwm_autoscale(true);    // Needed for stealthChop
+  // driver2.en_spreadCycle(true);   // false = StealthChop / true = SpreadCycle
+  driver2.en_spreadCycle(false);
+
+  stepper2.setCurrentPositionInSteps(0);
+  stepper2.setSpeedInStepsPerSecond(400);
+  stepper2.setAccelerationInStepsPerSecondPerSecond(400);
+  #endif
+
+  #ifdef MOTOR_3
+  pinMode(M3_STEP, OUTPUT);
+  pinMode(M3_DIR, OUTPUT);
+  pinMode(M3_EN, OUTPUT);
+
+  stepper3.connectToPins(M3_STEP, M3_DIR);
+
+  driver3.begin();
+  driver3.toff(5);          // Enables Driver in Software
+  driver3.rms_current(MOTOR_CURRENT);
+  driver3.microsteps(MOTOR_MICROSTEP);
+
+  // driver3.pwm_autoscale(true);    // Needed for stealthChop
+  // driver3.en_spreadCycle(true);   // false = StealthChop / true = SpreadCycle
+  driver3.en_spreadCycle(false);
+
+  stepper3.setCurrentPositionInSteps(0);
+  stepper3.setSpeedInStepsPerSecond(400);
+  stepper3.setAccelerationInStepsPerSecondPerSecond(400);
+  #endif
 }
 
 
 /*
  * Function to Hardware Enable Motor
- * PMOS Contol
+ * GPIO Control - Pull-up at MCU
 */
 void enable_motor(int motorID){
+  switch(motorID){
+    case 1:
+      pinMode(M1_EN, LOW);
+      break;
+    case 2:
+      pinMode(M2_EN, LOW);
+      break;
+  }
+}
+
+
+/*
+ * Function to Hardware Disable Motor
+ * GPIO Control - Pull-up at MCU
+*/
+void disable_motor(int motorID){
   switch(motorID){
     case 1:
       pinMode(M1_EN, HIGH);
@@ -81,12 +144,62 @@ void enable_motor(int motorID){
   }
 }
 
+/*
+ * Function to Turn ON Valve
+ * NMOS Control - Pulled-down at Gate
+*/
+void valve_ON(){
+  pinMode(VALVE_CTLR, HIGH);
+}
+
+/*
+ * Function to Turn OFF Valve
+ * NMOS Control - Pulled-down at Gate
+*/
+void valve_OFF(){
+  pinMode(VALVE_CTLR, LOW);
+}
+
+/*
+ * Funtion to rotate Motor 1 by param angle
+ * param[in] angle
+*/
+void rotate_motor1_by_angle(long angle){
+  long curr_pos = stepper1.getCurrentPositionInSteps();
+  long target_pos = curr_pos - (angle * STEPS_PER_REVOLUTION/360);
+  stepper1.moveToPositionInSteps(target_pos);
+}
+
+void init_serial(){
+  Serial1.setRX(17u);
+  Serial1.setTX(16u);
+  Serial1.begin(9600);
+}
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(5000);
 
 }
+
+
+// Required Packages and Board
+/*
+ * Board
+ --------------------
+ * PICO             : https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
+
+ * Library
+ --------------------
+ * SpeedyStepper    : https://github.com/Stan-Reifel/SpeedyStepper
+ * TMCStepper       : https://github.com/teemuatlut/TMCStepper 
+*/
